e1efc64a1b173d2edaf46f552c1a3cf44a37863a
Alexander Sknarin <asknarin@dkfx.lan@wl05.linux.dkfx.lan>
2017-09-11 16:06:19 +0300

    Houdini start is now ready to use as console application

--- projectManager/HoudiniStart.py
+++ projectManager/HoudiniStart.py
@@ -5,116 +5,89 @@ import sys
 import os
 
 # Get variables
-projectName = "chernobyl"
-episodeName = "ep02"
-shotName    = "ep02-" + sys.argv[1]
-jobName     = "lighting"
-
-
+projectName = sys.argv[1]
+episodeName = sys.argv[2]
+shotName    = sys.argv[3]
+jobName     = sys.argv[4]
 
 
 # project structure: OLD
 oldProjects = ["SKIF", "IKARIA", "SALUT"]
 newProjects = ["chernobyl"]
 
-if(projectName in oldProjects):
-    # Generate variables
-    PRJ = os.path.join("/projects", projectName)
-
-    HOUDINI_OTLSCAN_PATH = '/prefs/houdini/LIGHTING_PREFS/otls:/projects/'+projectName+'/3D/OTL:&'
-    RAT = os.path.join(PRJ, "3D", "DATA", "RAT")
-    RSTEX = os.path.join(PRJ, "3D", "DATA", "RSTEX")
-    LIGHTING = os.path.join(PRJ, "3D", "LIGHTING", episodeName, shotName)
-    FX = os.path.join(PRJ, "3D", "FX", episodeName, shotName)
-    ANIMATION = os.path.join(PRJ, "3D", "DATA", episodeName, shotName, "ANIMATION")
-    FXCACHE = os.path.join(PRJ, "3D", "DATA", episodeName, shotName, "FX")
-    RENDER = os.path.join(PRJ, "SRC", "RENDER", episodeName, shotName, jobName)
-    TRACKING = os.path.join(PRJ, "3D", "TRACKING", episodeName, shotName)
-
-    print "PRJ:                  " + PRJ
-    print "HOUDINI_OTLSCAN_PATH: " + HOUDINI_OTLSCAN_PATH
-    print "RAT:                  " + RAT
-    print "RSTEX:                " + RSTEX
-    print "LIGHTING:             " + LIGHTING
-    print "FX:                   " + FX
-    print "ANIMATION:            " + ANIMATION
-    print "FXCACHE:              " + FXCACHE
-    print "RENDER:               " + RENDER
-    print "TRACKING:             " + TRACKING
-
-
-    # Set Variables
-    os.environ["PRJ"] = PRJ
-    os.environ["HOUDINI_OTLSCAN_PATH"] = HOUDINI_OTLSCAN_PATH
-    os.environ["RAT"] = RAT
-    os.environ["RSTEX"] = RSTEX
-    os.environ["LIGHTING"] = LIGHTING
-    os.environ["FX"] = FX
-    os.environ["ANIMATION"] = ANIMATION
-    os.environ["FXCACHE"] = FXCACHE
-    os.environ["RENDER"] = RENDER
-
-    # Redshift variables
-    os.environ["REDSHIFT_COREDATAPATH"] = "/opt/redshift"
-    os.environ["HOUDINI_DSO_ERROR"] = "2"
-    os.environ["PATH"] = "/opt/redshift/bin:$PATH"
-    os.environ["HOUDINI_PATH"] = "/opt/redshift/redshift4houdini/16.0.534:&"
-
-    houdiniProcess = subprocess.Popen("/opt/hfs16.0.539/bin/houdinifx", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, bufsize=1)
-    with houdiniProcess.stdout:
-        for line in iter(houdiniProcess.stdout.readline, b''):
-            sys.stdout.write(line)
-    houdiniProcess.wait()
-
 if(projectName in newProjects):
     # Generate variables
+    # PRJ project root path
     PRJ = os.path.join("/fd/projects", projectName)
+    # OTL scan path
+    HOUDINI_OTLSCAN_PATH = '/fd/lib/houdini/otls:' + PRJ + '/lib/houdini/otls:&'
+    # JOB  path to current shot root
+    JOB = os.path.join(PRJ, "episodes", episodeName, shotName)
+    # SCENES path to current folder for scenes
+    SCENES = os.path.join(JOB, "scenes")
 
-    HOUDINI_OTLSCAN_PATH = '/prefs/houdini/LIGHTING_PREFS/otls:' + '/fd/projects/chernobyl/lib/houdini/otls:&'
-    RAT = os.path.join(PRJ, "episodes", episodeName, shotName, "data", "tex")
-    RSTEX = os.path.join(PRJ, "episodes", episodeName, shotName, "data", "tex")
-    LIGHTING = os.path.join(PRJ, "episodes", episodeName, shotName, "scenes", "lighting")
-    FX = os.path.join(PRJ, "episodes", episodeName, shotName, "scenes", "fx")
-    ANIMATION = os.path.join(PRJ, "episodes", episodeName, shotName, "scenes", "animation")
-    CACHE = os.path.join(PRJ, "episodes", episodeName, shotName, "data", "cache")
-    FXCACHE = os.path.join(PRJ, "3D", "DATA", episodeName, shotName, "FX")
-    RENDER = os.path.join(PRJ, "episodes", episodeName, shotName, "render", jobName)
-    TRACKING = os.path.join(PRJ, "3D", "TRACKING", episodeName, shotName)
-    ASSETS = "/fd/projects/chernobyl/assets/"
+    # RENDER folder for renders dependent on jobname
+    RENDER = ""
+    if(jobName == "animation"):
+        RENDER = os.path.join(JOB, "render", "animation")
+    elif(jobName == "fx"):
+        RENDER = os.path.join(JOB, "render", "fx")
+    elif (jobName == "layout"):
+        RENDER = os.path.join(JOB, "render", "layout")
+    elif (jobName == "lighting"):
+        RENDER = os.path.join(JOB, "render", "lighting")
+    elif (jobName == "previs"):
+        RENDER = os.path.join(JOB, "render", "previs")
+    elif (jobName == "tracking"):
+        RENDER = os.path.join(JOB, "render", "tracking")
 
-    print "PRJ:                  " + PRJ
+    # DATA path to data folders
+    DATA = os.path.join(JOB, "data")
+    CACHE = os.path.join(DATA, "cache")
+    ANIMCACHE = os.path.join(CACHE, "animation")
+    FXCACHE = os.path.join(CACHE, "fx")
+    CAMERA = os.path.join(CACHE, "tracking")
+    TEX = os.path.join(DATA, "tex")
+
+    ASSETS = os.path.join(PRJ, "assets")
+
+
     print "HOUDINI_OTLSCAN_PATH: " + HOUDINI_OTLSCAN_PATH
-    print "RAT:                  " + RAT
-    print "RSTEX:                " + RSTEX
-    print "LIGHTING:             " + LIGHTING
-    print "FX:                   " + FX
-    print "ANIMATION:            " + ANIMATION
-    print "CACHE:                " + CACHE
-    print "ASSETS:               " + ASSETS
+    print "PRJ:                  " + PRJ
+    print "JOB:                  " + JOB
+    print "SCENES:               " + SCENES
     print "RENDER:               " + RENDER
-    print "TRACKING:             " + TRACKING
+    print "DATA:                 " + DATA
+    print "CACHE:                " + CACHE
+    print "ANIMCACHE:            " + ANIMCACHE
+    print "FXCACHE:              " + FXCACHE
+    print "CAMERA:               " + CAMERA
+    print "TEX:                  " + TEX
+    print "ASSETS:               " + ASSETS
 
 
     # Set Variables
-    os.environ["PRJ"] = PRJ
     os.environ["HOUDINI_OTLSCAN_PATH"] = HOUDINI_OTLSCAN_PATH
-    os.environ["RAT"] = RAT
-    os.environ["RSTEX"] = RSTEX
-    os.environ["LIGHTING"] = LIGHTING
-    os.environ["FX"] = FX
-    os.environ["ANIMATION"] = ANIMATION
-    os.environ["FXCACHE"] = FXCACHE
-    os.environ["CACHE"] = CACHE
-    os.environ["ASSETS"] = ASSETS
+    os.environ["PRJ"] = PRJ
+    os.environ["JOB"] = JOB
+    os.environ["SCENES"] = SCENES
     os.environ["RENDER"] = RENDER
+    os.environ["DATA"] = DATA
+    os.environ["CACHE"] = CACHE
+    os.environ["ANIMCACHE"] = ANIMCACHE
+    os.environ["FXCACHE"] = FXCACHE
+    os.environ["CAMERA"] = CAMERA
+    os.environ["TEX"] = TEX
+    os.environ["ASSETS"] = ASSETS
+
 
     # Redshift variables
     os.environ["REDSHIFT_COREDATAPATH"] = "/opt/redshift"
     os.environ["HOUDINI_DSO_ERROR"] = "2"
     os.environ["PATH"] = "/opt/redshift/bin:$PATH"
-    os.environ["HOUDINI_PATH"] = "/opt/redshift/redshift4houdini/16.0.534:&"
+    os.environ["HOUDINI_PATH"] = "/opt/redshift/redshift4houdini/16.0.633:&"
 
-    houdiniProcess = subprocess.Popen("/software/hfs/16.0.671/bin/houdinifx", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, bufsize=1)
+    houdiniProcess = subprocess.Popen("/opt/hfs16.0.671/bin/houdinifx", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, bufsize=1)
     with houdiniProcess.stdout:
         for line in iter(houdiniProcess.stdout.readline, b''):
             sys.stdout.write(line)
