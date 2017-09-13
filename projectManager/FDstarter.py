e614863959bfffbeb6aaf5490b2b6bc87589aa2f
asknarin@dkfx.lan <filmdirectionTD@gmail.com>
2017-09-13 19:47:48 +0300

    FDstarter added

---
+++ projectManager/FDstarter.py
@@ -0,0 +1,449 @@
+#!/usr/bin/python
+from PySide import QtCore, QtGui
+import sys
+import os
+
+import cerebroRead
+import filters
+
+import subprocess
+
+
+# --------------- GUI class description --------------------------------------------------------------------------------
+
+class FdstarterGUI (QtGui.QWidget):
+
+    def __init__(self):
+        super(FdstarterGUI, self).__init__()
+        self.setupUI()
+
+        host = 'fs.dkfx.lan'
+        port = '5432'
+
+        self.db = cerebroRead.database.Database(host, port)
+        self.cerebroProject = cerebroRead.CrProject()
+        self.model = QtGui.QStandardItemModel()
+
+        self.selectedProject = ""
+        self.selectedEpisode = ""
+        self.selectedShot = ""
+        self.selectedJob = ""
+
+        self.bgColors = ["NavajoWhite1", "LightSalmon1", "Grey93", "MistyRose1", "Cornsilk1", "Honeydew2", "LightSteelBlue1", "LightYellow3", "LightSkyBlue1", "SkyBlue1", "Wheat1", "DarkOliveGreen1", "LightCyan3", "White"]
+
+
+
+    def setupUI(self):
+        # Add UI Elements--------------------------------------
+        # SCENES:
+        # First Column:
+        # Login
+        labelUname = QtGui.QLabel("Username: ")
+        labelPass  = QtGui.QLabel("Password: ")
+        lineInputUname = QtGui.QLineEdit()
+        lineInputUname.setAutoFillBackground(True)
+        lineInputPass  = QtGui.QLineEdit()
+        lineInputPass.setAutoFillBackground(True)
+        self.buttonLogin    = QtGui.QPushButton("Login to Cerebro")
+        iconLogin = QtGui.QIcon(QtGui.QPixmap('/home/asknarin/py_projects/icons/login.png'))
+        self.buttonLogin.setIcon(iconLogin)
+
+        self.buttonStartHoudini = QtGui.QPushButton("Start Houdini")
+        iconHoudini = QtGui.QIcon(QtGui.QPixmap('/home/asknarin/py_projects/icons/houdini_logo.png'))
+        self.buttonStartHoudini.setIcon(iconHoudini)
+
+        self.buttonStartNuke = QtGui.QPushButton("Start Nuke")
+        iconNuke = QtGui.QIcon(QtGui.QPixmap('/home/asknarin/py_projects/icons/nukeIcon.png'))
+        self.buttonStartNuke.setIcon(iconNuke)
+
+        self.buttonStartMaya = QtGui.QPushButton("Start Maya")
+        iconMaya = QtGui.QIcon(QtGui.QPixmap('/home/asknarin/py_projects/icons/hsMaya.png'))
+        self.buttonStartMaya.setIcon(iconMaya)
+
+
+        hboxUname = QtGui.QHBoxLayout()
+        hboxPass = QtGui.QHBoxLayout()
+
+        loginColumnLayout = QtGui.QVBoxLayout()
+
+        hboxUname.addWidget(labelUname, 0)
+        hboxUname.addWidget(lineInputUname, 1)
+
+        hboxPass.addWidget(labelPass, 0)
+        hboxPass.addWidget(lineInputPass, 1)
+
+        loginColumnLayout.addItem(hboxUname)
+        loginColumnLayout.addItem(hboxPass)
+        loginColumnLayout.addWidget(self.buttonLogin, 2)
+        loginColumnLayout.addWidget(self.buttonStartHoudini, 3)
+        loginColumnLayout.addWidget(self.buttonStartNuke, 4)
+        loginColumnLayout.addWidget(self.buttonStartMaya, 5)
+
+        loginColumnLayout.addStretch()
+
+        self.iconGreen = QtGui.QIcon(QtGui.QPixmap('icons/GREEN_light.png'))
+        self.iconRed = QtGui.QIcon(QtGui.QPixmap('icons/RED_light.png'))
+
+        # Second column
+        # Projects List
+
+        labelProjects = QtGui.QLabel("Select a Project: ")
+        self.listProjects  = QtGui.QListWidget()
+
+        projectsColumnLayout = QtGui.QVBoxLayout()
+        projectsColumnLayout.addWidget(labelProjects, 0)
+        projectsColumnLayout.addWidget(self.listProjects, 1)
+
+        # Third column
+        # Episodes List
+        labelEpisodes = QtGui.QLabel("Episodes: ")
+        self.listEpisodes = QtGui.QListWidget()
+        labelShots = QtGui.QLabel("Shots: ")
+        self.listShots = QtGui.QListWidget()
+        labelJobs = QtGui.QLabel("Jobs: ")
+        self.listJobs = QtGui.QListWidget()
+
+        episodesColumnLayout = QtGui.QVBoxLayout()
+        episodesColumnLayout.addWidget(labelEpisodes, 0)
+        episodesColumnLayout.addWidget(self.listEpisodes, 1)
+
+        shotsColumnLayout = QtGui.QVBoxLayout()
+        shotsColumnLayout.addWidget(labelShots, 0)
+        shotsColumnLayout.addWidget(self.listShots, 1)
+
+        jobsColumnLayout = QtGui.QVBoxLayout()
+        jobsColumnLayout.addWidget(labelJobs, 0)
+        jobsColumnLayout.addWidget(self.listJobs, 1)
+
+
+        episodesGlobalLayout = QtGui.QHBoxLayout()
+        episodesGlobalLayout.addItem(episodesColumnLayout)
+        episodesGlobalLayout.addItem(shotsColumnLayout)
+        episodesGlobalLayout.addItem(jobsColumnLayout)
+
+        # ASSETS
+        # Assets tree view
+        labelAssetsTree = QtGui.QLabel("Asset Tree:")
+        self.assetsTreeView = QtGui.QTreeView()
+        self.assetsTreeView.setUniformRowHeights(True)
+        self.assetsTreeView.setAnimated(True)
+        self.assetsTreeView.setUniformRowHeights(True)
+
+
+        labelAssetJobsTree = QtGui.QLabel("Jobs List:")
+        self.assetJobsList  = QtGui.QListWidget()
+
+        assetTreeLayout = QtGui.QVBoxLayout()
+        assetTreeLayout.addWidget(labelAssetsTree, 0)
+        assetTreeLayout.addWidget(self.assetsTreeView, 1)
+
+        assetJobsLayout = QtGui.QVBoxLayout()
+        assetJobsLayout.addWidget(labelAssetJobsTree, 0)
+        assetJobsLayout.addWidget(self.assetJobsList, 1)
+
+        assetsGlobalLayout = QtGui.QHBoxLayout()
+        assetsGlobalLayout.addItem(assetTreeLayout)
+        assetsGlobalLayout.addItem(assetJobsLayout)
+
+        # Tab Widget for Scenes And Assets
+        assetEpisodeTabs = QtGui.QTabWidget()
+
+        # Tab for scenes:
+        episodesTab = QtGui.QWidget()
+        episodesTab.setLayout(episodesGlobalLayout)
+        episodesTab.setAutoFillBackground(True)
+
+        # Tab for assets
+        assetsTab = QtGui.QWidget()
+        assetsTab.setLayout(assetsGlobalLayout)
+        assetsTab.setAutoFillBackground(True)
+
+
+        # Add tabs
+        assetEpisodeTabs.addTab(episodesTab, "Episodes")
+        assetEpisodeTabs.addTab(assetsTab, "Assets")
+
+        # Global layout
+        globalLayout = QtGui.QHBoxLayout()
+        globalLayout.addItem(loginColumnLayout)
+        globalLayout.addItem(projectsColumnLayout)
+        globalLayout.addWidget(assetEpisodeTabs, 2)
+
+        # StatusLayout
+        self.statusBar = QtGui.QStatusBar()
+
+        StatusLayout = QtGui.QVBoxLayout()
+        StatusLayout.addItem(globalLayout)
+        StatusLayout.addWidget(self.statusBar, 1)
+        StatusLayout.setStretch(1, 0)
+
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(0, 0, 0))
+        self.statusBar.setPalette(palette)
+
+        self.statusBar.showMessage("Push Login to Cerebro/Get Projects List and Select Project from list ...")
+
+        #Connect slot and widgets
+        self.buttonLogin.clicked.connect(self.LoginToCerebro)
+
+        self.listProjects.itemClicked.connect(self.ProjectClicked)
+        self.listProjects.itemEntered.connect(self.ProjectClicked)
+        #
+        self.listEpisodes.itemClicked.connect(self.EpisodeClicked)
+        self.listEpisodes.itemEntered.connect(self.EpisodeClicked)
+        #
+        self.listShots.itemClicked.connect(self.ShotClicked)
+        self.listShots.itemEntered.connect(self.ShotClicked)
+        #
+        self.listJobs.itemClicked.connect(self.JobClicked)
+        self.listJobs.itemEntered.connect(self.JobClicked)
+        #
+        self.assetsTreeView.clicked.connect(self.AssetClicked)
+        self.assetsTreeView.entered.connect(self.AssetClicked)
+        #
+        self.assetJobsList.itemClicked.connect(self.AssetJobClicked)
+        self.assetJobsList.itemEntered.connect(self.AssetJobClicked)
+
+        #
+        self.buttonStartHoudini.clicked.connect(self.StartHoudini)
+
+        #general window setup---------------------------------
+
+        self.setLayout(StatusLayout)
+        self.setGeometry(900, 300, 1200, 600)
+        self.setWindowTitle("FilmDirectionFX Software Starter")
+        self.show()
+
+    def LoginToCerebro(self):
+        #login to cerebro with pass and login
+        user = 'asknarin'
+        password = 'evv250584'
+
+        # clear list widgets adn assets tree widget
+        self.listProjects.clear()
+        self.listEpisodes.clear()
+        self.listShots.clear()
+        self.listJobs.clear()
+        self.model.clear()
+
+        # Clear list of selected item names
+        self.selectedProject = ""
+        self.selectedEpisode = ""
+        self.selectedShot = ""
+        self.selectedJob = ""
+
+        # login
+        if self.db.connect_from_cerebro_client() != 0:
+            self.db.connect(user, password)
+
+        projectsList = cerebroRead.GetRootProjects(self.db)
+
+        filteredProjectsList = filters.FilterProjects(projectsList)
+        filteredProjectsList.sort()
+
+        for project in filteredProjectsList:
+            self.listProjects.addItem(project)
+
+        self.statusBar.showMessage("Select Project:")
+
+    def ProjectClicked(self, item):
+        # Clear
+        self.listEpisodes.clear()
+        self.listShots.clear()
+        self.listJobs.clear()
+        self.model.clear()
+        self.assetJobsList.clear()
+
+        self.selectedProject = ""
+        self.selectedEpisode = ""
+        self.selectedShot = ""
+        self.selectedJob = ""
+
+
+        # Get selected project
+        self.selectedProject = item.text()
+
+        # EPISODES
+        # Fill episodes and sort them
+        episodes = cerebroRead.GetEpisodes(self.selectedProject, self.db)
+        episodes = filters.FilterScenes(episodes)
+        episodes.sort()
+
+        # Fill  List widget
+        for episode in episodes:
+            self.listEpisodes.addItem(episode)
+
+        # ASSETS
+        # Fill assets and sort them
+        assets = cerebroRead.GetRootAssets(self.selectedProject, self.db)
+        assets.sort()
+
+        self.assetsTreeView.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+
+        self.model.setHorizontalHeaderLabels(["Name", "Type", "URL"])
+        self.assetsTreeView.setModel(self.model)
+        self.assetsTreeView.setColumnWidth(0, 200)
+        self.assetsTreeView.setColumnWidth(1, 130)
+        self.assetJobsList.clear()
+
+        for asset in assets:
+            assetItemName = QtGui.QStandardItem(asset)
+            assetItemType = QtGui.QStandardItem("Asset")
+            assetItemURL = QtGui.QStandardItem(self.selectedProject + "/assets/" + asset)
+            self.model.appendRow([assetItemName, assetItemType, assetItemURL])
+
+        self.statusBar.showMessage("Select Episode or Asset:")
+
+    def EpisodeClicked(self, item):
+        # Clear
+        self.listShots.clear()
+        self.listJobs.clear()
+        self.assetJobsList.clear()
+
+        # Clear all asset selection
+        self.assetsTreeView.clearSelection()
+        self.assetJobsList.clearSelection()
+
+        self.selectedEpisode = ""
+        self.selectedShot = ""
+        self.selectedJob = ""
+
+        # Get Selected Episode
+        self.selectedEpisode = item.text()
+
+        # Fill shots and sort
+        shots = cerebroRead.GetShots(self.selectedProject, self.selectedEpisode, self.db)
+        shots = filters.FilterShots(shots)
+        shots.sort()
+
+        for shot in shots:
+            self.listShots.addItem(shot)
+
+        self.statusBar.showMessage("Select Shot:")
+
+    def ShotClicked(self, item):
+        # Clear
+        self.listJobs.clear()
+        self.assetJobsList.clear()
+
+        # Clear all asset selection
+        self.assetsTreeView.clearSelection()
+        self.assetJobsList.clearSelection()
+
+        self.selectedShot = ""
+
+        # Get Selected shot
+        self.selectedShot = item.text()
+
+        # Fill jobs
+        jobs = cerebroRead.GetJobs(self.selectedProject, self.selectedEpisode, self.selectedShot, self.db)
+        #jobs = filters.FilterShotJobs(jobs)
+        jobs.sort()
+
+        for job in jobs:
+            self.listJobs.addItem(job)
+        self.statusBar.showMessage("Select Job:")
+
+        self.selectedJob = ""
+
+
+    def JobClicked(self, item):
+        # Clear
+        self.selectedJob = item.text()
+
+
+    def AssetClicked(self, item):
+        # Clear Selections
+        self.listEpisodes.clearSelection()
+        self.listShots.clearSelection()
+        self.listJobs.clearSelection()
+
+        # Clear lists
+        self.listShots.clear()
+        self.listJobs.clear()
+        self.assetJobsList.clear()
+
+        self.selectedShot = ""
+        self.selectedJob = ""
+
+        self.selectedEpisode = "assets"
+        try:
+            treeItem = self.model.itemFromIndex(item)
+            self.selectedShot = treeItem.text()
+        except IndexError:
+            print ""
+
+        # Fill Asset Jobs list
+        assetJobs = cerebroRead.GetAssetJobs(self.selectedProject, self.selectedShot, self.db)
+        assetJobs.sort()
+
+        for job in assetJobs:
+            self.assetJobsList.addItem(job)
+
+
+    def AssetJobClicked(self, item):
+        #Clear Selections
+        self.listEpisodes.clearSelection()
+        self.listShots.clearSelection()
+        self.listJobs.clearSelection()
+
+        # Clear lists
+        self.listShots.clear()
+        self.listJobs.clear()
+
+        self.selectedJob = item.text()
+
+
+
+    def StartHoudini(self):
+        import random
+        import time
+        random.seed(int(time.time()))
+        color = self.bgColors[random.randrange(len(self.bgColors))]
+
+        # Check if project selected
+        if (self.selectedProject != ""):
+            # Check if assets or episides selected
+            if(self.selectedEpisode == "assets"):
+                # Check if asset and asset job are selected
+                if(self.selectedShot != ""):
+                    if(self.selectedJob != ""):
+                        # Check for jobs inappropriate for Houdini assets
+                        if(self.selectedJob not in "comp concept"):
+                            print "Starting Houdini for asset"
+                            windowTitle = self.selectedProject.lower() + "_" + self.selectedEpisode + "_" + self.selectedShot + "_" + self.selectedJob + "_ASSET"
+                            args = self.selectedProject.lower() + " " + self.selectedEpisode + " " + self.selectedShot + " " + self.selectedJob
+                            subprocess.Popen("xterm -xrm 'XTerm*selectToClipboard: true' -geometry 170x55+120+120 -fa 'Monospace' -fs 8 -T " + windowTitle + " -bg " + color + " -sb -sl 100000 -e /fd/lib/python/projectManager/HoudiniStart.py " + args, shell=True)
+                        else:
+                            self.statusBar.showMessage("Select othe job: " + self.selectedJob + " is inappropriate for Houdini")
+                    else:
+                        self.statusBar.showMessage("Select Asset Job:")
+                else:
+                    self.statusBar.showMessage("Select Asset:")
+            elif(self.selectedEpisode != ""):
+                # Assume that episode selected
+                # Check if shot and job are selected
+                if (self.selectedShot != ""):
+                    if (self.selectedJob != ""):
+                        # Check for jobs inappropriate for Houdini shots
+                        if (self.selectedJob not in "comp tracking"):
+                            print "Starting Houdini for shot"
+                            windowTitle = self.selectedProject.lower() + "_" + self.selectedEpisode + "_" + self.selectedShot + "_" + self.selectedJob + "_SHOT"
+                            args = self.selectedProject.lower() + " " + self.selectedEpisode + " " + self.selectedShot + " " + self.selectedJob
+                            subprocess.Popen("xterm -xrm 'XTerm*selectToClipboard: true' -geometry 170x55+120+120 -fa 'Monospace' -fs 8 -T " + windowTitle + " -bg " + color + " -sb -sl 100000 -e /fd/lib/python/projectManager/HoudiniStart.py " + args, shell=True)
+                        else:
+                            self.statusBar.showMessage("Select othe job: " + self.selectedJob + " is inappropriate for Houdini")
+                    else:
+                        self.statusBar.showMessage("Select Job:")
+                else:
+                    self.statusBar.showMessage("Select Shot:")
+            else:
+                self.statusBar.showMessage("Select Episode or Asset:")
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    #app.setStyle("motif")
+    frame = FdstarterGUI()
+    frame.show()
+    app.exec_()
